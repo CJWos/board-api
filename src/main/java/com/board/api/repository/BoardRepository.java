8cf5e385a5e8e1d73a8f39a138c240cf0b609e37
CJWos <viego113@gmail.com>
2022-12-08 20:41:55 +0900

    .

--- src/main/java/com/board/api/repository/BoardRepository.java
+++ src/main/java/com/board/api/repository/BoardRepository.java
@@ -2,7 +2,6 @@ package com.board.api.repository;
 
 import com.board.api.entity.Board;
 import org.springframework.data.jpa.repository.JpaRepository;
-
 import java.util.Optional;
 
 public interface BoardRepository extends JpaRepository<Board, Long> {
